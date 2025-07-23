2796e5127cb69cfc6e86a2372cc154ab43065b66
Setsiri <setsiriaunsiam@gmail.com>
2025-07-24 02:11:17 +0700

    sign in-up page

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:local_shopee/pages/sign_in.dart';
+import 'package:local_shopee/pages/sign_up.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Center(child: Text('Profile Page')));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('Profile Page'),
+            SignInButton(),
+            SignUpButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignInButton extends StatelessWidget {
+  const SignInButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const SignInPage()),
+        );
+      },
+      child: const Text('Sign In'),
+    );
+  }
+}
+
+class SignUpButton extends StatelessWidget {
+  const SignUpButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const SignUpPage()),
+        );
+      },
+      child: const Text('Sign Up'),
+    );
   }
 }
