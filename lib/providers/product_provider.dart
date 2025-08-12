7c172a744f401cd0de8509e3dc56c26d02707128
Charif <hifrook.zen5@gmail.com>
2025-08-12 16:45:47 +0700

    change to db

--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -4,29 +4,16 @@ import 'package:flutter/material.dart';
 import '../models/product.dart';
 import 'package:decimal/decimal.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ProductProvider with ChangeNotifier {
 
   final collection = FirebaseFirestore.instance.collection("products");
-  
-  final List<Product> _products = [
-    Product(id: '1', name: 'Item 1', location: "คอหงศ์", price: Decimal.fromInt(200), rating: Decimal.fromInt(5)),
-    Product(id: '2', name: 'Item 2', location: "คอหงศ์", price: Decimal.fromInt(120), rating: Decimal.parse("3.5")),
-    Product(id: '3', name: 'Item 3', location: "คอหงศ์", price: Decimal.fromInt(333), rating: Decimal.parse("2.3")),
-    Product(id: '4', name: 'Item 4', location: "คอหงศ์", price: Decimal.fromInt(444), rating: Decimal.fromInt(5)),
-    Product(id: '5', name: 'Item 5', location: "คอหงศ์", price: Decimal.fromInt(555), rating: Decimal.fromInt(5)),
-    Product(id: '6', name: 'Item 6', location: "คอหงศ์", price: Decimal.fromInt(666), rating: Decimal.parse("3.3")),
-    Product(id: '7', name: 'Item 7', location: "คอหงศ์", price: Decimal.fromInt(123), rating: Decimal.parse("4.7")),
-    Product(id: '8', name: 'Item 8', location: "คอหงศ์", price: Decimal.fromInt(321), rating: Decimal.fromInt(5)),
-  ];
-  
-  List<Product> get products => _products;
-  List<Product> get favoriteProducts => _products.where((p) => p.isFavorite).toList();
-
+  final user = FirebaseAuth.instance.currentUser;
 
   final List<Product> _productsDb = [];
   List<Product> get productsDb => _productsDb;
-  List<Product> get favoriteProductsDb => _productsDb.where((p) => p.isFavorite).toList();
+  List<Product> get favoriteProductsDb => _productsDb.where((p) => p.id == user?.uid && p.isFavorite).toList();
 
   ProductProvider() {
     loadProductsFromDb();
@@ -55,7 +42,7 @@ class ProductProvider with ChangeNotifier {
   Future<void> loadProductsFromDb() async {
     try {
       final snapshot = await collection.get();
-      _products.clear();
+      _productsDb.clear();
       for (var doc in snapshot.docs) {
         final data = doc.data();
         _productsDb.add(Product(
@@ -90,6 +77,7 @@ class ProductProvider with ChangeNotifier {
       final isFavorite = doc.data()?['isFavorite'] ?? false;
       try {
         await item.update({'isFavorite': _productsDb[index].isFavorite});
+        print('fav by ${user?.uid} : ${_productsDb[index].isFavorite}');
       } catch (e) {
         print('Error updating favorite status: $e');
         _productsDb[index].isFavorite = isFavorite;
@@ -99,38 +87,26 @@ class ProductProvider with ChangeNotifier {
       print('Document does not exist');
     }
   }
-  
-
-
-
-
-
-  // void toggleFavorite(String id) {
-  //   final index = _products.indexWhere((p) => p.id == id);
-  //   if (index != -1) {
-  //     _products[index].isFavorite = !_products[index].isFavorite;
-  //     notifyListeners();
-  //   }
-  // }
-
-
 
   addProduct() async {
     try {
       final product = Product(
-        id: DateTime.now().toString(),
+        id: user?.uid ?? 'null',
         name: 'New Product',
         location: 'คอหงส์',
         price: Decimal.fromInt(100),
         rating: Decimal.fromInt(4),
       );
       await collection.add({
+        'id': user?.uid ?? 'null',
         'name': product.name,
         'location': product.location,
         'price': product.price.toString(),
         'rating': product.rating.toString(),
         'isFavorite': false,
       });
+      _productsDb.add(product);
+      print('Product added: ${product.name} by ${user?.uid}');
     } catch (e) {
       print('Error adding product: $e');
     }
